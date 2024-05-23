1e4fb3f52933f053a96e437df36e3060300aae27
haozexiang <1521183864@qq.com>
2024-05-23 18:56:20 +0800

    提交+1

--- background.js
+++ background.js
@@ -1,17 +1,65 @@
-const extensions = 'https://www.baidu.com'
-const webstore = 'https://www.baidu.com'
+const offscreenCanvas = new OffscreenCanvas(16, 16);
+const ctx = offscreenCanvas.getContext('2d');
+// 设置初始颜色
+let color = 'rgb(255, 0, 0)';
+// 定义颜色循环
+const colors = ['rgb(255, 0, 0)', 'rgb(255, 128, 0)', 'rgb(255, 255, 0)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)', 'rgb(128, 0, 128)'];
+let colorIndex = 0;
+// 绘制函数
+function draw() {
+    // 清空画布
+    ctx.clearRect(0, 0, 16, 16);
+    // 绘制霓虹灯效果
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, 16, 16);
+    // 更新颜色
+    colorIndex = (colorIndex + 1) % colors.length;
+    color = colors[colorIndex];
+    const imageData = ctx.getImageData(0, 0, 16, 16);
+    console.log(imageData)
+    chrome.action.setIcon({imageData: imageData}, () => { /* ... */ });
 
+}
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.setBadgeText({
+        text: "🐒",
+    });
+    chrome.action.setBadgeBackgroundColor(
+        {color: '#ffffff'},  // Green
+        () => { /* ... */ },
+    );
+    // 开始动画
+    // setInterval(()=>{
+    //     draw();
+    // },1000)
+});
+// const extensions = 'https://www.baidu.com'
+//
+/*
+* 如果扩展程序操作指定了在用户点击当前标签页时显示的弹出式窗口，则不会发送 action.onClicked 事件
+* */
 chrome.action.onClicked.addListener(async (tab) => {
-    if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-        // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+    console.log("This is a background script!")
+    draw();
+    console.log(tab.url.startsWith('https://www.baidu.com'))
+    if (tab.url.startsWith('https://www.baidu.com')) {
         const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-        // Next state will always be the opposite
-        const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-
-        // Set the action badge to the next state
+        const nextState = prevState === '🐒' ? '👌🏻' : '🐒'
         await chrome.action.setBadgeText({
             tabId: tab.id,
             text: nextState,
         });
+        if(nextState ==='👌🏻'){
+            await chrome.scripting.insertCSS({
+                files: ["index.css"],
+                target: { tabId: tab.id,allFrames : true, },
+            });
+        }else {
+            await chrome.scripting.removeCSS({
+                files: ["index.css"],
+                target: { tabId: tab.id,allFrames : true, },
+            });
+        }
+
     }
 });
--- scripts/content.js
+++ scripts/content.js
@@ -4,19 +4,18 @@ if(hotList){
     // 循环获取hotList中的li标签中的span 文本
     const spans = hotList.querySelectorAll("li span");
     spans.forEach(span => {
-        console.log(span.innerText);
         hotText.push(span.innerText);
     });
     // 创建div元素 将 hotText中的文本添加到div元素中 最后将div元素添加到body中
-    let divText = document.getElementById('hotText');
-    if(!divText){
-        divText = document.createElement("div");
-    }
-    divText.innerText = hotText.join("-");
-    divText.id = "hotText";
-    divText.style.position = "fixed";
-    divText.style.top = "0";
-    divText.style.right = "0";
-    divText.style.zIndex = '999'
-    document.body.appendChild(divText);
+    // let divText = document.getElementById('hotText');
+    // if(!divText){
+    //     divText = document.createElement("div");
+    // }
+    // divText.innerText = hotText.join("-");
+    // divText.id = "hotText";
+    // divText.style.position = "fixed";
+    // divText.style.top = "0";
+    // divText.style.right = "0";
+    // divText.style.zIndex = '999'
+    // document.body.appendChild(divText);
 }
